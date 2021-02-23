b07e6ca9258f6cd8251475750ea3094b6fcafbc9
Berat Koçak <42033267+Reitnos@users.noreply.github.com>
2021-02-23 20:09:41 +0300

    trial

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  debugger;
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
